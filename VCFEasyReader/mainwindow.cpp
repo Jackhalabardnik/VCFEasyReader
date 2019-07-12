3b0857a14386eb165f36ed69072f848ecf8918f3
Jackhalabardnik <jackowojcio@wp.pl>
2019-07-12 16:24:57 +0200

    almost GUI

--- VCFEasyReader/mainwindow.cpp
+++ VCFEasyReader/mainwindow.cpp
@@ -2,9 +2,136 @@
 #include <iostream>
 
 MainWindow::MainWindow()
-: m_button("Hello World")
 {
-  set_border_width(10);
-  add(m_button);
-  m_button.show();
+  setButtons();
+  setLabels();
+  setMenus();
+  setButtonBox();
+  setOutputBox();
+  setMainGrid();
+  setWindow();
+  showAll();
+}
+
+void MainWindow::setButtons()
+{
+	openFile_button.set_label("OpenFile");
+	print_button.set_label("Print");
+	saveToTextFile_button.set_label("saveToTextFile");
+}
+
+void MainWindow::setButtonBox()
+{
+	
+}
+
+void MainWindow::setOutputBox()
+{
+	auto scrolledWindow = Gtk::manage(new Gtk::ScrolledWindow);
+	scrolledWindow->set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
+	scrolledWindow->add(textView);
+	scrolledWindow->set_size_request(-1,200);
+	outputBox.set_orientation(Gtk::ORIENTATION_VERTICAL);
+	outputBox.pack_start(*scrolledWindow, Gtk::PACK_EXPAND_WIDGET);
+	outputBox.pack_start(pathLabel, Gtk::PACK_SHRINK);
+	mainBox.pack_start(outputBox, Gtk::PACK_EXPAND_WIDGET);
+}
+
+void MainWindow::setMainGrid()
+{
+	mainBox.set_orientation(Gtk::ORIENTATION_VERTICAL);
+	add(mainBox);
+}
+
+void MainWindow::setWindow()
+{
+	//set_border_width(10);
+}
+void MainWindow::showAll()
+{
+	outputBox.show_all();
+	mainBox.show();
+}
+
+void MainWindow::setLabels()
+{
+	pathLabel.set_text("Path to selected file");
+}
+
+void MainWindow::setMenus()
+{
+	menubarActionGroup = Gio::SimpleActionGroup::create();
+	menubarActionGroup->add_action("opennew", sigc::mem_fun(*this, &MainWindow::openNewFile));
+	menubarActionGroup->add_action("print");
+	menubarActionGroup->add_action("export");
+	viewAG = Gio::SimpleActionGroup::create();
+	viewAG->add_action_radio_integer("minimal", sigc::mem_fun(*this, &MainWindow::changeView), 1);
+	viewAG->add_action_radio_integer("list", sigc::mem_fun(*this, &MainWindow::changeView), 2);
+	insert_action_group("menu", menubarActionGroup);
+	insert_action_group("view", viewAG);
+	
+	Glib::RefPtr<Gtk::Builder> refBuilder = Gtk::Builder::create();
+	
+	const char* ui_info =
+  "<interface>"
+  "  <menu id='menubar'>"
+  "    <submenu>"
+  "      <attribute name='label' translatable='yes'>File</attribute>"
+  "      <section>"
+  "        <item>"
+  "          <attribute name='label' translatable='yes'>Open</attribute>"
+  "          <attribute name='action'>menu.opennew</attribute>"
+  "          <attribute name='accel'>&lt;Primary&gt;o</attribute>"
+  "        </item>"
+  "      </section>"
+  "      <section>"
+  "        <item>"
+  "          <attribute name='label' translatable='yes'>Print</attribute>"
+  "          <attribute name='action'>menu.print</attribute>"
+  "          <attribute name='accel'>&lt;Primary&gt;p</attribute>"
+  "        </item>"
+  "      </section>"
+  "      <section>"
+  "        <item>"
+  "          <attribute name='label' translatable='yes'>Export</attribute>"
+  "          <attribute name='action'>menu.export</attribute>"
+  "          <attribute name='accel'>&lt;Primary&gt;i</attribute>"
+  "        </item>"
+  "      </section>"
+  "    </submenu>"
+  "    <submenu>"
+  "      <attribute name='label' translatable='yes'>View</attribute>"
+  "      <item>"
+  "        <attribute name='label' translatable='yes'>Minimal</attribute>"
+  "        <attribute name='action'>view.minimal</attribute>"
+  "          <attribute name='target'>1</attribute>"
+  "      </item>"
+  "      <item>"
+  "        <attribute name='label' translatable='yes'>List</attribute>"
+  "        <attribute name='action'>view.list</attribute>"
+  "          <attribute name='target'>2</attribute>"
+  "      </item>"
+  "    </submenu>"
+  "  </menu>"
+  "</interface>";
+	
+	refBuilder->add_from_string(ui_info);
+	
+	
+	Glib::RefPtr<Glib::Object> object = refBuilder->get_object("menubar");
+	Glib::RefPtr<Gio::Menu> gmenu = Glib::RefPtr<Gio::Menu>::cast_dynamic(object);
+	Gtk::MenuBar* pMenuBar = Gtk::manage(new Gtk::MenuBar(gmenu));
+	pMenuBar->show_all();
+	mainBox.pack_start(*pMenuBar, Gtk::PACK_SHRINK);
+}
+
+void MainWindow::openNewFile()
+{
+	std::cout << "Works\n";
+}
+
+void MainWindow::changeView(int mode)
+{
+	std::string mess = mode == 1 ? "minimal" : "list";
+	std::cout << "Current mode is " << mode << ".\n";
 }
--- VCFEasyReader/mainwindow.h
+++ VCFEasyReader/mainwindow.h
@@ -2,15 +2,32 @@
 #define GTKMM_MAINWINDOW_H
 
 #include <gtkmm.h>
+#include <iostream>
 
 class MainWindow : public Gtk::Window
 {
 
 public:
-  MainWindow();
+	MainWindow();
 
+private:
+	void showAll();
+	void setButtons();
+	void setLabels();
+	void setMenus();
+	void setButtonBox();
+	void setOutputBox();
+	void setMainGrid();
+	void setWindow();
+	void openNewFile();
+	void changeView(int mode);
 protected:
-  Gtk::Button m_button;
+	Gtk::Label pathLabel;
+	Gtk::Button openFile_button, print_button, saveToTextFile_button;
+	Gtk::TextView textView;
+	Gtk::Box outputBox, mainBox;
+	Gtk::Grid outputGrid;
+	Glib::RefPtr<Gio::SimpleActionGroup> menubarActionGroup, viewAG;
 };
 
 #endif
